9a75125849f1bb2f53cb26ba97e43523d3834175
Marek Cieslar <mcie@astrouw.edu.pl>
2016-01-26 18:53:12 +0100

    Started work on proper TOV. Moved old to "Almost".

--- ShootingRKTOV.cpp
+++ ShootingRKTOV.cpp
@@ -9,146 +9,230 @@ using namespace std;
 
 struct SInteriorSpaceTOV
 {
-	RealType fXi;
-	RealType fV;
-	RealType fTheta;
+	RealType fR;
+	RealType fM;
+	RealType fMProper;
+	RealType fP;
+	RealType fPhi;
 
-	RealType fDVDXi;
-	RealType fDThetaDXi;
+	RealType fDMDR;
+	RealType fDMProperDR;
+	RealType fDPDR;
+	RealType fDPhiDR;
 
-	void Add(const RealType fDXi, const RealType fDV, const RealType fDTheta);
-
+	void Zero();
 };
+
+
+void SInteriorSpaceTOV::Zero()
+{
+	fR=0.;
+	fM=0.;
+	fMProper=0.;
+	fP=0.;
+	fPhi=0.;
+
+	fDMDR=0.;
+	fDMProperDR=0.;
+	fDPDR=0.;
+	fDPhiDR=0.;
+}
+
+
+SInteriorSpaceTOV &operator+=(SInteriorSpaceTOV &Left, const SInteriorSpaceTOV &Right)
+{
+	Left.fR+=Right.fR;
+	Left.fM+=Right.fM;
+	Left.fMProper+=Right.fMProper;
+	Left.fP+=Right.fP;
+	Left.fPhi+=Right.fPhi;
+
+	Left.fDMDR+=Right.fDMDR;
+	Left.fDMProperDR+=Right.fDMProperDR;
+	Left.fDPDR+=Right.fDPDR;
+	Left.fDPhiDR+=Right.fDPhiDR;
+
+}
+
+SInteriorSpaceTOV operator+(const SInteriorSpaceTOV &Left, const SInteriorSpaceTOV &Right)
+{
+	SInteriorSpaceTOV Result(Left);
+	Result+=Right;
+	return Result;
+}
+
+
+SInteriorSpaceTOV operator/(const SInteriorSpaceTOV Point, const RealType fNumber)
+{
+	SInteriorSpaceTOV Result(Point);
+	Result.fR/=fNumber;
+	Result.fM/=fNumber;
+	Result.fMProper/=fNumber;
+	Result.fP/=fNumber;
+	Result.fPhi/=fNumber;
+
+	Result.fDMDR/=fNumber;
+	Result.fDMProperDR/=fNumber;
+	Result.fDPDR/=fNumber;
+	Result.fDPhiDR/=fNumber;
+	return Result;
+}
 
 ostream &operator<<( ostream &output, const SInteriorSpaceTOV &Point )
 {
 	output << scientific;
-	output << Point.fXi<<" "<<Point.fV<<" "<<Point.fTheta<<" "<<Point.fDVDXi<<" "<<Point.fDThetaDXi;
+	output << Point.fR<<" "<<Point.fM<<" "<<Point.fMProper<<" "<<Point.fP<<" "<<Point.fPhi<<" "<<Point.fDMDR<<" "<<Point.fDMProperDR<<" "<<Point.fDPDR<<" "<<Point.fDPhiDR;
 	return output;
-}
-
-void SInteriorSpaceTOV::Add(const RealType fDXi, const RealType fDV, const RealType fDTheta)
-{
-	fXi+=fDXi;
-	fV+=fDV;
-	fTheta+=fDTheta;
 }
 
 //how to impreve the name of this function???
 RealType MultiDimRelativeDifference(const SInteriorSpaceTOV A, const SInteriorSpaceTOV B)
 {
-	return sqrt(pow((B.fXi-A.fXi)/B.fXi, 2.) + pow((B.fV-A.fV)/B.fV, 2.) + pow((B.fTheta-A.fTheta)/B.fTheta, 2.) + pow((B.fDVDXi-A.fDVDXi)/B.fDVDXi, 2.) + pow((B.fDThetaDXi-A.fDThetaDXi)/B.fDThetaDXi, 2.));
+	return sqrt(pow((B.fM-A.fM)/B.fM, 2.) 
+                  + pow((B.fP-A.fP)/B.fP, 2.) 
+                  + pow((B.fPhi-A.fPhi)/B.fPhi, 2.) 
+                  + pow((B.fDMDR-A.fDMDR)/B.fDMDR, 2.) 
+                  + pow((B.fDPDR-A.fDPDR)/B.fDPDR, 2.) 
+                  + pow((B.fDPhiDR-A.fDPhiDR)/B.fDPhiDR, 2.));
 }
 
 bool isnan(const SInteriorSpaceTOV A)
 {
-	return ( isnan(A.fXi) || isnan(A.fV) || isnan(A.fTheta) || isnan(A.fDVDXi) || isnan(A.fDThetaDXi) );
+	return ( isnan(A.fR) || isnan(A.fP) || isnan(A.fM) || isnan(A.fMProper) || isnan(A.fPhi) || isnan(A.fDMDR) || isnan(A.fDMProperDR) || isnan(A.fDPhiDR) || isnan(A.fDPDR) );
 }
 
 class CPolytropeShooting
 {
 protected:
-	RealType m_VDerivative(const SInteriorSpaceTOV Point) const;
-	RealType m_ThetaDerivative(const SInteriorSpaceTOV Point) const;
+	RealType m_MDerivative(const SInteriorSpaceTOV Point) const;
+	RealType m_MProperDerivative(const SInteriorSpaceTOV Point) const;
+	RealType m_PDerivative(const SInteriorSpaceTOV Point) const;
+	RealType m_PhiDerivative(const SInteriorSpaceTOV Point) const;
 
 	SInteriorSpaceTOV m_ApproximateSolutionInZero(const RealType fEpsilon) const; 
+	SInteriorSpaceTOV m_PropagateRKMethod(const SInteriorSpaceTOV PreviousPoint, const RealType fDeltaR) const;
 
-	SInteriorSpaceTOV m_PropagateRKMethod(const SInteriorSpaceTOV PreviousPoint, const RealType fDeltaXi) const;
-
-	RealType m_MassTilda(const SInteriorSpaceTOV Point) const;
 public:
-	RealType fPolytropicIndex;
-	RealType fSigma;
+	RealType fPolytropicGamma;
+	RealType fRhoCentre;
 
 	void ComputeInterior();
 	void ComputeInteriorAdaptive();
-	void ComputeInteriorAdaptiveBisection();
 
-	CPolytropeShooting();
-	CPolytropeShooting(const RealType fN, const RealType fSig);
+	RealType ComputePhiConstant(const SInteriorSpaceTOV Point) const;
+
+	CPolytropeShooting(const RealType fGamma, const RealType fRhoC);
 	~CPolytropeShooting(){};
 
 };
 
-CPolytropeShooting::CPolytropeShooting()
-{
-	CPolytropeShooting(3.,0.5);///testing
-}
 
-CPolytropeShooting::CPolytropeShooting(const RealType fN, const RealType fSig)
+CPolytropeShooting::CPolytropeShooting(const RealType fGamma, const RealType fRhoC)
 {
-	fPolytropicIndex=fN;
-	fSigma=fSig;
+	fPolytropicGamma=fGamma;
+	fRhoCentre=fRhoC;
 }
 
 SInteriorSpaceTOV CPolytropeShooting::m_ApproximateSolutionInZero(const RealType fEpsilon) const
 {
 	SInteriorSpaceTOV Result;
-	Result.fXi=fEpsilon;
-	Result.fV=0.;
-	Result.fTheta=1.;
-	Result.fDVDXi=m_VDerivative(Result);
-	Result.fDThetaDXi=m_ThetaDerivative(Result);
+
+	Result.fR=fEpsilon;
+	Result.fM=0.;
+	Result.fP=0.;
+	Result.fPhi=1.;
+
+	Result.fDMDR=m_MDerivative(Result);
+	Result.fDMProperDR=m_MProperDerivative(Result);
+	Result.fDPDR=m_PDerivative(Result);
+	Result.fDPhiDR=m_PhiDerivative(Result);
+
 	return Result;
 }
 
 
-RealType CPolytropeShooting::m_VDerivative(const SInteriorSpaceTOV Point) const
+RealType CPolytropeShooting::m_MDerivative(const SInteriorSpaceTOV Point) const
 {
-	return Point.fXi*Point.fXi*pow(Point.fTheta,fPolytropicIndex);
+	return 0.;
 }
 
-RealType CPolytropeShooting::m_ThetaDerivative(const SInteriorSpaceTOV Point) const
+RealType CPolytropeShooting::m_MProperDerivative(const SInteriorSpaceTOV Point) const
 {
-	return -(1./(Point.fXi*Point.fXi))*(Point.fV + fSigma*pow(Point.fTheta,fPolytropicIndex+1)*Point.fXi*Point.fXi*Point.fXi  )*(1.+fSigma*Point.fTheta) / (1.-2.*fSigma*(fPolytropicIndex+1.)*Point.fV/Point.fXi ); 
+	return 0.;
 }
 
-SInteriorSpaceTOV CPolytropeShooting::m_PropagateRKMethod(const SInteriorSpaceTOV PreviousPoint, const RealType fDeltaXi) const
+RealType CPolytropeShooting::m_PDerivative(const SInteriorSpaceTOV Point) const
+{
+	return 0.;
+}
+
+RealType CPolytropeShooting::m_PhiDerivative(const SInteriorSpaceTOV Point) const
+{
+	return 0.;
+}
+
+SInteriorSpaceTOV CPolytropeShooting::m_PropagateRKMethod(const SInteriorSpaceTOV PreviousPoint, const RealType fDeltaR) const
 {
 	SInteriorSpaceTOV WorkPoint;
+	SInteriorSpaceTOV DWorkPoint1;
+	SInteriorSpaceTOV DWorkPoint2;
+	SInteriorSpaceTOV DWorkPoint3;
+	SInteriorSpaceTOV DWorkPoint4;
 
 	WorkPoint = PreviousPoint;
 
-	RealType fK1( fDeltaXi * m_VDerivative( WorkPoint ) );
-	RealType fL1( fDeltaXi * m_ThetaDerivative( WorkPoint ) );
+	DWorkPoint1.Zero();
+	DWorkPoint1.fR       = fDeltaR;
+	DWorkPoint1.fM       = fDeltaR * m_MDerivative( WorkPoint );
+	DWorkPoint1.fMProper = fDeltaR * m_MProperDerivative( WorkPoint );
+	DWorkPoint1.fP       = fDeltaR * m_PDerivative( WorkPoint );
+	DWorkPoint1.fPhi     = fDeltaR * m_PhiDerivative( WorkPoint );
 
 	WorkPoint = PreviousPoint;
-	WorkPoint.Add(fDeltaXi/2., fK1/2., fL1/2. );
+	WorkPoint += DWorkPoint1/2.; 
+//	WorkPoint.Add(fDeltaR/2., fM1/2., fP1/2., fPhi1/2. );
 
-	RealType fK2( fDeltaXi * m_VDerivative( WorkPoint ) );
-	RealType fL2( fDeltaXi * m_ThetaDerivative( WorkPoint ) );
-
-	WorkPoint = PreviousPoint;
-	WorkPoint.Add(fDeltaXi/2., fK2/2., fL2/2. );
-
-
-	RealType fK3( fDeltaXi * m_VDerivative( WorkPoint ) );
-	RealType fL3( fDeltaXi * m_ThetaDerivative( WorkPoint ) );
+	DWorkPoint2.Zero();
+	DWorkPoint2.fR       = fDeltaR;
+	DWorkPoint2.fM       = fDeltaR * m_MDerivative( WorkPoint );
+	DWorkPoint2.fMProper = fDeltaR * m_MProperDerivative( WorkPoint );
+	DWorkPoint2.fP       = fDeltaR * m_PDerivative( WorkPoint );
+	DWorkPoint2.fPhi     = fDeltaR * m_PhiDerivative( WorkPoint );
 
 	WorkPoint = PreviousPoint;
-	WorkPoint.Add(fDeltaXi, fK3, fL3 );
+	WorkPoint += DWorkPoint2/2.; 
 
-	RealType fK4( fDeltaXi * m_VDerivative( WorkPoint ) );
-	RealType fL4( fDeltaXi * m_ThetaDerivative( WorkPoint ) );
+	DWorkPoint3.Zero();
+	DWorkPoint3.fR       = fDeltaR;
+	DWorkPoint3.fM       = fDeltaR * m_MDerivative( WorkPoint );
+	DWorkPoint3.fMProper = fDeltaR * m_MProperDerivative( WorkPoint );
+	DWorkPoint3.fP       = fDeltaR * m_PDerivative( WorkPoint );
+	DWorkPoint3.fPhi     = fDeltaR * m_PhiDerivative( WorkPoint );
 
+	WorkPoint = PreviousPoint;
+	WorkPoint += DWorkPoint3; 
+
+	DWorkPoint4.Zero();
+	DWorkPoint4.fR       = fDeltaR;
+	DWorkPoint4.fM       = fDeltaR * m_MDerivative( WorkPoint );
+	DWorkPoint4.fMProper = fDeltaR * m_MProperDerivative( WorkPoint );
+	DWorkPoint4.fP       = fDeltaR * m_PDerivative( WorkPoint );
+	DWorkPoint4.fPhi     = fDeltaR * m_PhiDerivative( WorkPoint );
 
 	SInteriorSpaceTOV Result;
 
-	Result.fXi = PreviousPoint.fXi + fDeltaXi;
-	Result.fV = PreviousPoint.fV + fK1/6. + fK2/3. + fK3/3. +fK4/6.;
-	Result.fTheta = PreviousPoint.fTheta + fL1/6. + fL2/3. + fL3/3. +fL4/6.;
-	Result.fDVDXi =  m_VDerivative( Result );
-	Result.fDThetaDXi = m_ThetaDerivative( Result );
+	Result = PreviousPoint + DWorkPoint1/6. + DWorkPoint2/3. + DWorkPoint3/3. + DWorkPoint4/6.;
+	//Correcting fR:
+	Result.fR = PreviousPoint.fR + fDeltaR;
 
+	Result.fDMDR =  m_MDerivative( Result );
+	Result.fDMProperDR =  m_MProperDerivative( Result );
+	Result.fDPDR =  m_PDerivative( Result );
+	Result.fDPhiDR = m_PhiDerivative( Result );
 
 	return Result;
 }
 	
-RealType CPolytropeShooting::m_MassTilda(const SInteriorSpaceTOV Point) const
-{
-	return pow(fSigma,(3.-fPolytropicIndex)/2.)*Point.fV;
-}
-
 
 void CPolytropeShooting::ComputeInterior()
 {
@@ -158,14 +242,13 @@ void CPolytropeShooting::ComputeInterior()
 	const RealType fStep(1.e-4);
 	SInteriorSpaceTOV PointOfInterest;
 	PointOfInterest = m_ApproximateSolutionInZero(fAlmostZero);
-	cout<<PointOfInterest<<" "<<m_MassTilda(PointOfInterest)<<endl;
-	while (PointOfInterest.fTheta>fAlmostZero)
+	cout<<PointOfInterest<<endl;
+	while (PointOfInterest.fP>fAlmostZero)
 	{
 		PointOfInterest = m_PropagateRKMethod(PointOfInterest,fStep);
-		cout<<PointOfInterest<<" "<<m_MassTilda(PointOfInterest)<<endl;
+		cout<<PointOfInterest<<endl;
 	}
 }
-
 
 void CPolytropeShooting::ComputeInteriorAdaptive()
 {
@@ -175,25 +258,25 @@ void CPolytropeShooting::ComputeInteriorAdaptive()
 
 	SInteriorSpaceTOV PointOfInterest, Small, Big;
 	PointOfInterest = m_ApproximateSolutionInZero(fAlmostZero);
-	cout<<PointOfInterest<<" "<<m_MassTilda(PointOfInterest)<<" "<<fStep<<endl;
-	while (PointOfInterest.fTheta>fAlmostZero)
+	cout<<PointOfInterest<<" "<<fStep<<endl;
+	while (PointOfInterest.fP>fAlmostZero)
 	{
 		do
 		{
 			fStep /= 2.;
 			Small = m_PropagateRKMethod( m_PropagateRKMethod(PointOfInterest,fStep/2.) , fStep/2.);
 			Big   = m_PropagateRKMethod( PointOfInterest,fStep);
-			//cout<<" "<<Small<<" "<<m_MassTilda(Small)<<" "<<fStep/2.<<endl;
-			//cout<<" "<<Big<<" "<<m_MassTilda(Big)<<" "<<fStep<<endl;
+			//cout<<" "<<Small<<" "<<fStep/2.<<endl;
+			//cout<<" "<<Big<<" "<<fStep<<endl;
 			//cout<<" "<<MultiDimRelativeDifference(Small,Big) <<" "<< fEnough<<endl;
 		}
-		while(  (MultiDimRelativeDifference(Small,Big) > fEnough) || isnan(Big)  ||  Big.fTheta < 0. );
+		while(  (MultiDimRelativeDifference(Small,Big) > fEnough) || isnan(Big)  ||  Big.fP < 0. );
 		PointOfInterest=Small;
-		cout<<PointOfInterest<<" "<<m_MassTilda(PointOfInterest)<<" "<<fStep<<endl;
+		cout<<PointOfInterest<<" "<<fStep<<endl;
 		fStep *= 4.;
 	}
 }
-
+#if 0
 void CPolytropeShooting::ComputeInteriorAdaptiveBisection()
 {
 	const RealType fAlmostZero ( 1e-5);
@@ -203,7 +286,7 @@ void CPolytropeShooting::ComputeInteriorAdaptiveBisection()
 	SInteriorSpaceTOV PointOfInterest, Small, Big;
 	PointOfInterest = m_ApproximateSolutionInZero(fAlmostZero);
 	cout<<PointOfInterest<<" "<<m_MassTilda(PointOfInterest)<<" "<<fStep<<endl;
-	while (PointOfInterest.fTheta>fAlmostZero)
+	while (PointOfInterest.fPhi>fAlmostZero)
 	{
 		do
 		{
@@ -214,17 +297,17 @@ void CPolytropeShooting::ComputeInteriorAdaptiveBisection()
 			//cout<<" "<<Big<<" "<<m_MassTilda(Big)<<" "<<fStep<<endl;
 			//cout<<" "<<MultiDimRelativeDifference(Small,Big) <<" "<< fEnough<<endl;
 		}
-		while( (MultiDimRelativeDifference(Small,Big) > fEnough) || isnan(Big) || Big.fTheta < 0. );
+		while( (MultiDimRelativeDifference(Small,Big) > fEnough) || isnan(Big) || Big.fPhi < 0. );
 		PointOfInterest=Small;
 		cout<<PointOfInterest<<" "<<m_MassTilda(PointOfInterest)<<" "<<fStep<<endl;
 		fStep *= 4.;
 	}
 	fStep /= 8.;
-	//Find the such fXi so the fTheta<0	
+	//Find the such fR so the fPhi<0	
 	SInteriorSpaceTOV Left, Right;
 	Left=PointOfInterest; //>0
 	//cout<<Left<<" "<<m_MassTilda(PointOfInterest)<<" "<<fStep<<endl;
-	while(PointOfInterest.fTheta>0.)
+	while(PointOfInterest.fPhi>0.)
 	{
 		PointOfInterest=m_PropagateRKMethod( PointOfInterest,fStep);
 	}
@@ -233,11 +316,11 @@ void CPolytropeShooting::ComputeInteriorAdaptiveBisection()
 
 	const RealType fZero(1e-15);
 	//Do the bisection
-	while(Left.fTheta>fZero)
+	while(Left.fPhi>fZero)
 	{
-		fStep=(Right.fXi-Left.fXi)/2.;
+		fStep=(Right.fR-Left.fR)/2.;
 		PointOfInterest=m_PropagateRKMethod( Left,fStep);
-		if(PointOfInterest.fTheta>0.) Left=PointOfInterest;
+		if(PointOfInterest.fPhi>0.) Left=PointOfInterest;
 		else Right=PointOfInterest;
 
 	}
@@ -245,18 +328,18 @@ void CPolytropeShooting::ComputeInteriorAdaptiveBisection()
 }
 
 
-
+#endif
 int main(int argc, char** argv)
 {
 	if(argc != 3)
 	{
 		cout<<"Wrong arguments"<<endl;
-		cout<<"./PolyTOVShooting fPolytropicIndex fSigma"<<endl;
+		cout<<"./PolyTOVShooting fPolytropicGamma fRhoCentre"<<endl;
 		return 1;
 	}
 
 	CPolytropeShooting Poly(atof(argv[1]),atof(argv[2]));
-	Poly.ComputeInteriorAdaptiveBisection();
+	Poly.ComputeInteriorAdaptive();
 	//Poly.ComputeInterior();
 
 
